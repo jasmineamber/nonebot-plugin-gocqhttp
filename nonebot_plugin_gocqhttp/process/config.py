cdb807579956c5d59bdfc04d5cc129ddf722fa68
jasmineamber <jasmineamber@aliyun.com>
2022-07-02 16:39:24 +0800

    增加http监听

--- nonebot_plugin_gocqhttp/process/config.py
+++ nonebot_plugin_gocqhttp/process/config.py
@@ -46,6 +46,7 @@ class AccountConfigHelper:
                 "account": self.account,
                 "server_address": f"ws://127.0.0.1:{driver_config.port}/onebot/v11/ws",
                 "access_token": onebot_config.onebot_access_token or "",
+                "http_server_address": f"ws://127.0.0.1:{driver_config.http_port}/onebot/v11/ws",
             },
         )
         return self.config_path.write_text(rendered_string, encoding="utf-8")
